1ccf97806ac82c017e5fafdb97530d7f64fed689
Raven Eyex <raveneyex@gmail.com>
2016-11-26 23:51:43 -0500

    A Pentagram. Yay.

--- 5.pentagram/drawScript.js
+++ 5.pentagram/drawScript.js
@@ -1,16 +1,16 @@
-class Circle {
+class Pentagram {
   constructor({canvasId, strokeColor}){
     this.fillColor = strokeColor;
     this.canvasId = canvasId;
     this.animationFrameId = 0;
   }
 
-  atomicStep(init, radius, backwards) {
+  atomicStep(radius, backwards) {
     this.context.beginPath();
     let done = this.drawCircle(radius, backwards);
     this.step++;
     if (done){
-      this.context.closePath();
+      //this.context.closePath();
       this.step = 0;
     }
     return done;
@@ -19,23 +19,67 @@ class Circle {
   oneStep() {
 
     if (!this.first && !this.second) {
-      this.first = this.atomicStep(this.firstRun, 250, false)
+      this.first = this.atomicStep(250, false)
       if (this.firstRun) {
           this.firstRun = false;
       }
     }
 
     if(this.first && !this.second) {
-      this.context.strokeStyle = '#FFFFFF';
-      this.second = this.atomicStep(this.secondRun, 220, true);
+      this.second = this.atomicStep(220, true);
       if (this.secondRun) {
         this.secondRun = false;
       }
     }
 
     if (this.first && this.second) {
+      this.drawLine1();
+      this.drawLine2();
+      this.drawLine3();
+      this.drawLine4();
+      this.drawLine5();
       this.end();
     }
+  }
+
+  drawLine1(){
+    this.context.beginPath();
+    this.context.moveTo(300, 520);
+    this.context.lineTo(147, 147);
+    this.context.stroke();
+    this.context.closePath();
+  }
+
+  drawLine2(){
+    this.context.beginPath();
+    this.context.moveTo(147, 147);
+    this.context.lineTo(498, 400);
+    this.context.stroke();
+    this.context.closePath();
+  }
+
+  drawLine3(){
+    this.context.beginPath();
+    this.context.moveTo(498, 400);
+    this.context.lineTo(104, 400);
+    this.context.stroke();
+    this.context.closePath();
+  }
+
+  drawLine4(){
+    this.context.beginPath();
+    this.context.moveTo(104, 400);//451 144
+    this.context.lineTo(451, 144);
+    this.context.stroke();
+    this.context.closePath();
+  }
+
+  drawLine5(){
+    this.context.beginPath();
+    this.context.moveTo(451, 144);//4
+    this.context.lineTo(300, 520);
+    this.context.stroke();
+    this.context.closePath();
   }
 
   drawCircle(radius, counter){
@@ -58,7 +102,7 @@ class Circle {
 
   end() {
     cancelAnimationFrame(this.animationFrameId);
-    this.startup();
+    //this.startup();
   }
 
   startup() {
@@ -66,8 +110,15 @@ class Circle {
     this.context = document.getElementById(this.canvasId).getContext('2d');
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+    this.canvas.onmousemove = function(event) {
+      let x = event.clientX,
+          y = event.clientY;
+      document.getElementById('coordinates').innerHTML="Coordinates: (" + x + "," + y + ")";
+      console.log("Event:", event);
+    }
+
     this.context.strokeStyle = this.fillColor ;
-    this.context.fillStyle = this.fillColor;
+    //this.context.fillStyle = this.fillColor;
 
     this.startAngle = 0;
     this.endAngle = 2 * Math.PI;
@@ -88,5 +139,5 @@ class Circle {
 
 }
 
-const baphomet = new Circle({canvasId:'canvas', strokeColor: '#FF0000'});
+const baphomet = new Pentagram({canvasId:'canvas', strokeColor: '#FF0000'});
 baphomet.startup();
